a6a138d121ce9be1f04fb004c75502d7b1a1f270
DKendle <dina.kendle@gmail.com>
2021-06-20 11:35:31 -0400

    commit tree 40e0fff7c946cb727fb436de049da1f453e5275b

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+class Polygon {
+    constructor(array){
+        this.side1 = array[0]
+        this.side2 = array[1]
+        this.side3 = array[2]
+    }
+    get countSides(){
+
+    }
+    get perimeter(){
+
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid(){
+
+    }
+}
+
+class Square extends Polygon {
+    
+}
